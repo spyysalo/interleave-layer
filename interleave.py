f417d3dbc012f6500bc58e38c638a5fb202f33ae
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2016-11-09 19:16:56 +0000

    Attempt as masking (#2)

--- interleave.py
+++ interleave.py
@@ -26,13 +26,15 @@ class Interleave(Layer):
 
     (first dimension for batch not shown.)
     """
-    
+
+    def __init__(self, **kwargs):
+        self.supports_masking = True
+        super(Interleave, self).__init__(**kwargs)
+
     def call(self, inputs, mask=None):
         if type(inputs) is not list or len(inputs) != 2:
             raise ValueError('Interleave must be called with a list '
                              'of two tensors')
-        if any(m for m in mask if m is not None):
-            raise NotImplementedError('mask for Interleave')
         a, b = inputs
         if K.ndim(a) != 3 or K.ndim(b) != 3:
             raise ValueError('Interleaved tensors must have ndim 3')
@@ -44,12 +46,17 @@ class Interleave(Layer):
         a_shape, b_shape = input_shape
         return (a_shape[0], b_shape[1], 2*a_shape[2]+b_shape[2])
 
+    def compute_mask(self, inputs, masks=None):
+        if masks is None:
+            return None
+        return masks[1]
+
 
 if __name__ == '__main__':
     # Example
     import numpy as np
     from keras.models import Model
-    from keras.layers import Input, Embedding, LSTM
+    from keras.layers import Input, Embedding, LSTM, Masking
     
     # 5-dim word embeddings, 4-dim dep embeddings
     we = np.arange(10).repeat(5).reshape((-1, 5))
@@ -58,23 +65,19 @@ if __name__ == '__main__':
     # Inputs are sequences of three words and two dependencies
     w_in = Input(shape=(3,))
     d_in = Input(shape=(2,))
-    # w_emb = Embedding(we.shape[0], we.shape[1], weights=[we],
-    #                   name="L_Embedding_Word", mask_zero=True)(w_in)
-    # d_emb = Embedding(de.shape[0], de.shape[1], weights=[de],
-    #                   name="L_Embedding_DT", mask_zero=True)(d_in)
     w_emb = Embedding(we.shape[0], we.shape[1], weights=[we],
-                      name="L_Embedding_Word")(w_in)
+                      name="W_Emb", mask_zero=True)(w_in)
     d_emb = Embedding(de.shape[0], de.shape[1], weights=[de],
-                      name="L_Embedding_DT")(d_in)
-    lstm_words = LSTM(output_dim=2, activation=None,
-                      name="L_LSTM_Word", return_sequences=True)(w_emb)
-    lstm_dt = LSTM(output_dim=3, activation=None,
-                   name="L_LSTM_DT", return_sequences=True)(d_emb)
-    out = Interleave(name="OUT")([lstm_words, lstm_dt])
+                      name="D_Emb", mask_zero=True)(d_in)
+    w_lstm = LSTM(output_dim=2, activation=None,
+                  name="W_LSTM", return_sequences=True)(w_emb)
+    d_lstm = LSTM(output_dim=3, activation=None,
+                  name="D_LSTM", return_sequences=True)(d_emb)
+    out = Interleave(name="Intlv")([w_lstm, d_lstm])
     model = Model(input=[w_in, d_in], output=out)
     model.compile('adam', 'mse')
     
-    sentence1_words = np.asanyarray([[0,1,2]])
-    sentence_1_deps = np.asanyarray([[1,0]])
+    words = np.asanyarray([[0,1,2]])
+    deps = np.asanyarray([[0,1]])
 
-    print(model.predict([sentence1_words, sentence_1_deps]))
+    print(model.predict([words, deps]))
